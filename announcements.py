1460102467a9b6eaea3c6add632f8ea710ff213c
mrsimonsen <johnsimonsen@weber.edu>
2022-06-19 12:40:22 -0600

    questions added to skip thanksgiving, winter, and spring breaks

--- announcements.py
+++ announcements.py
@@ -1,13 +1,21 @@
 import datetime, shutil, os
-WEEKS = 35
+WEEKS = 40
 start = int(input("Enter first announcemnt date in August:\n"))
 date = datetime.date(year = datetime.date.today().year, month=8, day=start)
+thank = datetime.date(year = date.year+1, month=11, day=int(input("What does does thanksgiving break start in November>\n")))
+winter = datetime.date(year=datetime.date.today().year, month=12,day=int(input("What day does winter break start in December?\n")))
+spring = datetime.date(year=date.year+1, month=4, day=int(input("What day does spring break start in April?\n")))
 try:
 	os.mkdir("announcements")
 except FileExistsError:
 	shutil.rmtree("announcements")
 	os.mkdir("announcements")
 os.chdir("announcements")
+dates = [thank]
+for i in range(0,15,7):
+	dates.append(winter + datetime.timedelta(days=i))
+dates.append(spring + datetime.timedelta(days=2))
 for i in range(1, WEEKS+1):
-	shutil.copyfile("../blank.odt", f"{i:02} - {date.strftime('%b %d')}")
-	date = date + datetime.timedelta(days=7)
+	if date not in dates:
+		shutil.copyfile("../blank.odt", f"{i:02} - {date.strftime('%b %d')}.odt")
+	date += datetime.timedelta(days=7)
